251742b462759ea9de7a5e445538bfb97948eee0
KonradJam <acidburn.pl@gmail.com>
2026-06-09 14:35:33 +0200

    Add recorder section and styles to the app

---
+++ src/app/components/recorderSextion.js
@@ -0,0 +1,14 @@
+export const recorderSection = () => {
+    return/*html*/`
+        <aside class="recorder-panel">
+            <div class="btn-group">
+            <button class="btn-group__btn">
+                <span class="btn-group__text">&#9673;</span>
+                <span class="btn-group__text">REC</span>
+            </button>
+            <button class="btn-group__btn">&#9209;</button>
+            <button class="btn-group__btn">&#11123;</button>
+            </div>
+        </aside>
+    `;
+};
